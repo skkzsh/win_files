e88f7d58ff30c75ccf35e3af076b8c541075207b
skkzsh <skkzsh@users.noreply.github.com>
2016-10-17 01:46:56 +0900

    Delete gitmodule.bat

--- usr/setup/gitmodule.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SETLOCAL
-
-:: git submodule add https://gist.github.com/908117.git home/_nya_d/public_repos/git
-
-:: git submodule add https://github.com/dahlbyk/posh-git.git User/Documents/WindowsPowerShell/Modules/posh-git
-
-ENDLOCAL
-EXIT /B 0
